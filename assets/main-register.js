710c4d458d77554f7bb917b66d9313f6d0a1d380
marlonlaquindanum <marlon.laquindanum06@gmail.com>
2024-07-25 11:26:14 +0100

    HFISB-126: MR amends

--- assets/main-register.js
+++ assets/main-register.js
@@ -1,10 +1,11 @@
-const form_submit = document.querySelector('#create_customer button');
-const password_field = document.querySelector('#RegisterForm-password');
-const confirm_password_field = document.querySelector('#RegisterForm-confirm-password');
 const confirm_password_error = document.querySelector('#RegisterForm-confirm-password-error');
 
-form_submit.addEventListener('click', (e) => {
-  if (password_field.value === confirm_password_field.value) confirm_password_error.classList.add('hide');
+document.querySelector('#create_customer button').addEventListener('click', (e) => {
+  if (
+    document.querySelector('#RegisterForm-password').value ===
+    document.querySelector('#RegisterForm-confirm-password').value
+  )
+    confirm_password_error.classList.add('hide');
   else {
     e.preventDefault();
     confirm_password_error.classList.remove('hide');
